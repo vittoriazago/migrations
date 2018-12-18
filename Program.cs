deffdabeffc3a7775b3fa41d79d1d1af87a4c231
Vittoria Zago <vzago@paschoalotto.com.br>
2018-12-18 14:32:34 -0200

    Iniciando projeto para teste com migrations

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EntityCore2_MigrationsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

785b7ba4e64689b9af3aefc4898e79f22578737b
Vittoria Zago <vzago@paschoalotto.com.br>
2018-12-19 15:12:06 -0200

    Adicionando contexto inicial para migrations

---
+++ Context/BloggingContext.cs
@@ -0,0 +1,19 @@
+﻿using EntityCore2_MigrationsTest.Domains;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace EntityCore2_MigrationsTest
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=blogging.db");
+        }
+    }
+    
+}
+    
---
+++ Domains/Blog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityCore2_MigrationsTest.Domains
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+
+        public ICollection<Post> Posts { get; set; }
+    }
+}
